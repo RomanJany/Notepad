3d368e6a80a5c29b5ee4cb55b97493b5d67b38e2
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-10-16 17:45:13 +0200

    TextFile properties

--- Model/TextFile.cs
+++ Model/TextFile.cs
@@ -8,5 +8,34 @@ namespace Notepad.Model
 {
     public class TextFile
     {
+        private string _text;
+
+        public string Text 
+        { 
+            get 
+            { 
+                return _text; 
+            } 
+            set
+            {
+                _text = value;
+            }
+        }
+
+        private string _fileName;
+
+        public string FileName
+        { 
+            get 
+            { 
+                return _fileName; 
+            } 
+            set
+            {
+                _fileName = value;
+            }
+        }
+
+        public bool IsSaved { get; private set; }
     }
 }
