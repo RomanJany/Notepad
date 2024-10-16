a383e30aae2d48a6e8a9b02d38a1d0d5129ad7d1
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-10-16 19:17:35 +0200

    add NotepadSettings properties

--- Model/NotepadSettings.cs
+++ Model/NotepadSettings.cs
@@ -3,10 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace Notepad.Model
 {
     public class NotepadSettings
     {
+        public enum NotepadTheme
+        {
+            Light,
+            Dark
+        }
+
+        private NotepadTheme _theme;
+        public NotepadTheme Theme
+        {
+            get
+            {
+                return _theme
+            }
+            set
+            {
+                _theme = value;
+            }
+        }
+
+        private double _fontSize;
+        public double FontSize
+        {
+            get
+            {
+                return _fontSize;
+            }
+            set
+            {
+                _fontSize = value;
+            }
+        }
+
+        private FontFamily _font;
+        public FontFamily Font
+        {
+            get
+            {
+                return _font;
+            }
+            set
+            {
+                _font = value;
+            }
+        }
+
     }
 }
