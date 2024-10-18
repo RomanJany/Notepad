f768a5b4f8073d767a26e989a743b71dc7793897
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-10-18 21:46:40 +0200

    add OpenFileCommand

---
+++ Commands/OpenFileCommand.cs
@@ -0,0 +1,24 @@
+﻿using Notepad.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notepad.Commands
+{
+    public class OpenFileCommand : BaseCommand
+    {
+        private readonly MainViewModel _mainViewModel;
+
+        OpenFileCommand(MainViewModel mainViewModel)
+        {
+            _mainViewModel = mainViewModel;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
