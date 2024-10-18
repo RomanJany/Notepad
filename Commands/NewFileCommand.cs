348aba6c0080c703d423ffdc42bc3841a8873979
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-10-18 21:39:06 +0200

    NewFileCommand Execute implementation

--- Commands/NewFileCommand.cs
+++ Commands/NewFileCommand.cs
@@ -1,4 +1,5 @@
-﻿using Notepad.ViewModels;
+﻿using Notepad.Model;
+using Notepad.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,7 +19,7 @@ namespace Notepad.Commands
 
         public override void Execute(object? parameter)
         {
-            throw new NotImplementedException();
+            _mainViewModel.textFile = new TextFile();
         }
     }
 }
