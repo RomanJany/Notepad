38cd029968f79a33e56409d8c84355faf0644499
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-10-19 16:37:16 +0200

    PrintCommand - Execute implementation

--- Commands/PrintCommand.cs
+++ Commands/PrintCommand.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
 
 namespace Notepad.Commands
 {
@@ -18,7 +20,13 @@ namespace Notepad.Commands
 
         public override void Execute(object? parameter)
         {
-            throw new NotImplementedException();
+            PrintDialog printDialog = new PrintDialog();
+            if (printDialog.ShowDialog() == true)
+            {
+                FlowDocument document = new FlowDocument();
+                document.Blocks.Add(new Paragraph(new Run(_mainViewModel.Text)));
+                printDialog.PrintDocument(((IDocumentPaginatorSource) document).DocumentPaginator, "");
+            }
         }
     }
 }
