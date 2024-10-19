5e091df42647dd77f7fa604cef086f55d09b4571
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-10-19 13:41:06 +0200

    add SaveAsFileCommand

---
+++ Commands/SaveAsFileCommand.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Win32;
+using Notepad.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notepad.Commands
+{
+    public class SaveAsFileCommand : BaseCommand
+    {
+        private readonly MainViewModel _mainViewModel;
+
+        public SaveAsFileCommand(MainViewModel mainViewModel)
+        {
+            _mainViewModel = mainViewModel;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
