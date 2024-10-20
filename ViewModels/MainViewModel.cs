da16dcfcf67fc3013169ac442cda3b752215c50f
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-10-20 14:34:10 +0200

    add SetFontSizeCommand

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -24,6 +24,7 @@ namespace Notepad.ViewModels
             saveFileCommand = new SaveFileCommand(this);
             saveAsFileCommand = new SaveAsFileCommand(this);
             printCommand = new PrintCommand(this);
+            setFontSizeCommand = new SetFontSizeCommand(this);
         }
 
         private ObservableCollection<double> _fontSizeCollection = [8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72];
@@ -150,6 +151,7 @@ namespace Notepad.ViewModels
         public ICommand saveFileCommand { get; }
         public ICommand saveAsFileCommand { get; }
         public ICommand printCommand { get; }
+        public ICommand setFontSizeCommand { get; }
 
         void UpdateWindowTitle()
         {
