b4e21c375bd97452d3984f1d9b187ea04198e399
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-10-22 18:01:11 +0200

    TextBoxProperties - add methods

--- AttachedProperties/TextBoxProperties.cs
+++ AttachedProperties/TextBoxProperties.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Notepad.AttachedProperties
 {
-    public class TextBoxProperties
+    public static class TextBoxProperties
     {
+        public static readonly DependencyProperty CaretIndexProperty =
+        DependencyProperty.RegisterAttached(
+            "CaretIndex",
+            typeof(int),
+            typeof(TextBoxProperties),
+            new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, CaretIndexChanged));
+
+        public static int GetCaretIndex(DependencyObject obj)
+        {
+            return (int)obj.GetValue(CaretIndexProperty);
+        }
+
+        public static void SetCaretIndex(DependencyObject obj, int value)
+        {
+            obj.SetValue(CaretIndexProperty, value);
+        }
+
+        private static void CaretIndexChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox) obj;
+            if (textBox != null)
+            {
+                textBox.CaretIndex = (int)e.NewValue;
+            }
+        }
     }
 }
