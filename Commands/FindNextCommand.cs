fea5da0fbf9dc33b02a6c885c017a569afcdc527
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-10-23 21:38:03 +0200

    FindNextCommand - FindNext implementation

--- Commands/FindNextCommand.cs
+++ Commands/FindNextCommand.cs
@@ -31,7 +31,41 @@ namespace Notepad.Commands
 
         private void FindNext()
         {
-            throw new NotImplementedException();
+            if (_mainViewModel.Text.Contains(_mainViewModel.FindText))
+            {
+                int findIndex = 0;
+                string text = _mainViewModel.Text;
+                bool found = false;
+
+                // Search for string after CaretIndex
+                while (text.Contains(_mainViewModel.FindText))
+                {
+                    int currentIndex = text.IndexOf(_mainViewModel.FindText);
+                    text = text.Substring(currentIndex + 1);
+
+                    // Found next occurence
+                    if ((findIndex + currentIndex) > _mainViewModel.CaretIndex)
+                    {
+                        findIndex += currentIndex;
+                        found = true;
+                        break;
+                    }
+                    // Else continue
+                    else
+                    {
+                        findIndex += currentIndex + 1;
+                    }
+                }
+
+                // If the next occurence wasn't found -> select first occurence
+                if (!found)
+                {
+                    findIndex = _mainViewModel.Text.IndexOf(_mainViewModel.FindText);
+                }   
+
+                // Set the new index
+                _mainViewModel.CaretIndex = findIndex;
+            }
         }
 
         private void ReplaceNext()
